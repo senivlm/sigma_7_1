efd4bdc8ea32b728c3e735ec0c6ebde146e0da06
Oleh <olehbobeliak@gmail.com>
2021-10-14 17:41:17 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace sigma_7_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Translator t = new Translator("data.txt");
+            Console.WriteLine(t.Translate());
+        }
+    }
+}
---
+++ Translator.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace sigma_7_1
+{
+    class Translator
+    {
+        private string _text;
+        private List<string> _words;
+        private Dictionary<string, string> _dictionary = new Dictionary<string, string>();
+
+        public Translator(string filename)
+        {
+            try
+            {
+                using(StreamReader sr = new StreamReader(filename))
+                {
+                    _text = sr.ReadToEnd().ToLower();
+                    _words = _text.Split(new char[] { ' ', ',', '.', '!', '?' }, StringSplitOptions.RemoveEmptyEntries).ToList();
+                }
+            }
+            catch(FileNotFoundException)
+            {
+                Console.WriteLine("File was not found");
+                Environment.Exit(-1);
+            }
+            catch(OutOfMemoryException)
+            {
+                Console.WriteLine("Text is too large");
+                Environment.Exit(-1);
+            }
+
+            _dictionary.Add("i", "boy");
+            _dictionary.Add("go", "runs");
+            _dictionary.Add("to", "to");
+            _dictionary.Add("the", "the");
+            _dictionary.Add("school", "cinema");
+
+        }
+
+        public string Translate()
+        {
+            try
+            {
+                foreach (string word in _words)
+                {
+                    if(!_dictionary.ContainsKey(word))
+                    {
+                        Console.WriteLine($"\"{word}\" wasn't found in dictionary.\n" +
+                            "Enter the word you want to replace it with:\n");
+                        string val = Console.ReadLine();
+                        _dictionary.Add(word, val);
+                    }
+                }
+                foreach (var item in _dictionary)
+                {
+                    _text = _text.Replace(item.Key, item.Value);
+                }
+                
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            return _text;
+        }
+    }
+}
